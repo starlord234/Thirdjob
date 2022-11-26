90d7c5c4277a52a6c071abf0fa4a7c53916f62e6
starlord234 <jshaik830@gmail.com>
2022-11-26 18:45:33 +0530

    add my file

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+	public static void main(String[]args){
+        int myNum = 7;
+	int myNum1=9;
+  	String name = "jeelan";
+	System.out.println("print out the desired result in jenkins");
+	System.out.println(myNum+myNum1);
+	}
+	
+}
